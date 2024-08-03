3b1e6881ee54d340771744e6122aa656cb862e16
stonedev-app <54230974+stonedev-app@users.noreply.github.com>
2024-08-03 16:12:20 +0900

    feat: Fixed to play sound when button is pressed

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <Arduboy2.h>
 #include <ArduboyTones.h>
 
+// arduboy
 Arduboy2 arduboy;
+// sound
 ArduboyTones sound(arduboy.audio.enabled);
 
+// sound data (Do Re Mi Fa Sol La Ti Do)
 const uint16_t scale[] PROGMEM = {
   NOTE_C3,500, NOTE_D3,500, NOTE_E3,500, NOTE_F3,500,
   NOTE_G3,500, NOTE_A4,500, NOTE_B4,500, NOTE_C4,500,
@@ -11,10 +14,22 @@ const uint16_t scale[] PROGMEM = {
 };
 
 void setup() {
+  // initialize Arduboy
   arduboy.begin();
-  sound.tones(scale);
+  // set frame rate to 15FPS
+  arduboy.setFrameRate(15);
 }
 
 void loop() {
-}
+  // if it is not time for the next frame, go back to the start of the loop 
+  if (!arduboy.nextFrame()) return;
 
+  // use justPressed() as necessary to determine if a button was pressed
+  arduboy.pollButtons();
+
+  // A_BUTTON pressed
+  if(arduboy.justPressed(A_BUTTON)) {
+    // start sound
+    sound.tones(scale);
+  }
+}
