caa7e255b1d21f4818df4fe3fb6aa28c7ba776b4
stonedev-app <54230974+stonedev-app@users.noreply.github.com>
2024-08-04 17:36:39 +0900

    feat: Enabled button press sound playback while playing music using the ArduboyPlaytune library

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,22 @@
 #include <Arduboy2.h>
-#include <ArduboyTones.h>
+#include <ArduboyPlaytune.h>
 
 // arduboy
 Arduboy2 arduboy;
-// sound
-ArduboyTones sound(arduboy.audio.enabled);
+// audio
+ArduboyPlaytune tunes(arduboy.audio.enabled);
 
-// sound data (Do Re Mi Fa Sol La Ti Do)
-const uint16_t scale[] PROGMEM = {
-  NOTE_C3,500, NOTE_D3,500, NOTE_E3,500, NOTE_F3,500,
-  NOTE_G3,500, NOTE_A3,500, NOTE_B3,500, NOTE_C4,500,
-  TONES_END
+// sound data
+const byte score[] PROGMEM = {
+  0x90,48, 0x01,0xf4, 0x80, // Do  500ms
+  0x90,50, 0x01,0xf4, 0x80, // Re  500ms
+  0x90,52, 0x01,0xf4, 0x80, // Mi  500ms
+  0x90,53, 0x01,0xf4, 0x80, // Fa  500ms
+  0x90,55, 0x01,0xf4, 0x80, // Sol 500ms
+  0x90,57, 0x01,0xf4, 0x80, // La  500ms
+  0x90,59, 0x01,0xf4, 0x80, // Ti  500ms
+  0x90,60, 0x01,0xf4, 0x80, // Do  500ms
+  0xf0                      // End of score
 };
 
 void setup() {
@@ -18,6 +24,17 @@ void setup() {
   arduboy.begin();
   // set frame rate to 15FPS
   arduboy.setFrameRate(15);
+  
+  // audio setup
+  tunes.initChannel(PIN_SPEAKER_1);
+#ifndef AB_DEVKIT
+  // if not a Devkit
+  tunes.initChannel(PIN_SPEAKER_2);
+#else
+  // if it's a Devkit
+  tunes.initChannel(PIN_SPEAKER_1); // use the same pin for both channels
+  tunes.toneMuteScore(true);        // mute the score when a tone is sounding
+#endif
 }
 
 void loop() {
@@ -29,9 +46,16 @@ void loop() {
 
   // A_BUTTON pressed
   if(arduboy.justPressed(A_BUTTON)) {
-    if (!sound.playing()) {
-      // start sound
-      sound.tones(scale);
+    // if a score is not currently playing
+    if (!tunes.playing()) {
+      // play score
+      tunes.playScore(score);
     }
+  }
+
+  // B_BUTTON pressed
+  if (arduboy.justPressed(B_BUTTON)) {
+    // A4 100ms
+    tunes.tone(440, 100);
   }
 }
